0be3b84ff7018c6ddcdad56c0a08e8af46cf8e0b
JOBFER1 <jobfer@outlook.es>
2023-07-15 18:36:40 +0200

    Incluye el maven-checkstyle-plugin (corregido) + info (+ site y javadoc)
    - Site y javadoc ya estaban

--- src/main/java/com/curso/User.java
+++ src/main/java/com/curso/User.java
@@ -1,55 +1,84 @@
 package com.curso;
 /**
  * User object
- */
+ .*/
 public class User {
 
+    /**Comentario Javadoc para id.*/
     private Integer id;
+    /**Comentario Javadoc para username.*/
     private String username;
+    /**Comentario Javadoc para password.*/
     private String password;
+    /**Comentario Javadoc para email.*/
     private String email;
 
     /**
-     * Creates a new user
-     * @param username unique username of the user
-     * @param password password of the user
-     * @param email email address of the user
+     * Creates a new user.
+     * @param pUsername unique username of the user
+     * @param pPassword password of the user
+     * @param pEmail email address of the user
      */
-    public User(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
+    public User(final String pUsername, final String pPassword,
+            final String pEmail) {
+        this.username = pUsername;
+        this.password = pPassword;
+        this.email = pEmail;
     }
 
+    /**Comentario Javadoc para getId.
+     * @return El id
+     * */
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    /**Comentario Javadoc para setId.
+     * @param pId El id
+     * */
+    public void setId(final Integer pId) {
+        this.id = pId;
     }
 
+    /**Comentario Javadoc para getUsername.
+     * @return El username
+     * */
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    /**Comentario Javadoc para setUsername.
+     * @param pUsername El username
+     * */
+    public void setUsername(final String pUsername) {
+        this.username = pUsername;
     }
 
+    /**Comentario Javadoc para getPassword.
+     * @return El password
+     * */
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    /**Comentario Javadoc para setPassword.
+     * @param pPassword El password
+     * */
+    public void setPassword(final String pPassword) {
+        this.password = pPassword;
     }
 
+    /**Comentario Javadoc para getEmail.
+     * @return El email
+     * */
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    /**Comentario Javadoc para setEmail.
+     * @param pEmail El email
+     * */
+    public void setEmail(final String pEmail) {
+        this.email = pEmail;
     }
 }
